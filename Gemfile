9c49b6fbea9bb0f007b1737c08330cfcefa58ade
Michael Dao <mike@turing.io>
2019-03-30 12:02:16 -0600

    adds authentication

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'fast_jsonapi'
+gem 'figaro'
 
 group :development, :test do
   gem 'pry'
--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::HousesController < ApplicationController
   def show
-    result = House.where(name: params[:name].capitalize)
-    render json: HouseSerializer.new(result)
+    if params[:api_key] == ENV['API-KEY']
+      result = House.where(name: params[:name].capitalize)
+      render json: HouseSerializer.new(result)
+    else
+      render body: "Unauthorized", status: 401
+    end
   end
-end
