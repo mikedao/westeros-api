9ff23a7b353b6e1b000d4e0d7f2acc9068b1ef92
Michael Dao <mike@turing.io>
2019-03-30 11:48:31 -0600

    removes pry

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::HousesController < ApplicationController
   def show
-    binding.pry
     result = House.where(name: params[:name].capitalize)
     render json: HouseSerializer.new(result)
   end
